73d49c0b21b4091d1557e1c0e45f0652514a3ac3
asrim10 <asrimmsuwall@gmail.com>
2026-01-25 09:10:58 +0545

    hotel_dto

---
+++ src/dtos/hotel.dto.ts
@@ -0,0 +1,34 @@
+import z from "zod";
+import { HotelSchema } from "../types/hotel.types";
+
+export const CreateHotelDTO = HotelSchema.pick({
+  hotelname: true,
+  address: true,
+  city: true,
+  country: true,
+  rating: true,
+  description: true,
+  price: true,
+  availableRooms: true,
+});
+
+export type CreateHotelDTO = z.infer<typeof CreateHotelDTO>;
+
+export const UpdateHotelDTO = HotelSchema.pick({
+  hotelname: true,
+  address: true,
+  city: true,
+  country: true,
+  rating: true,
+  description: true,
+  price: true,
+  availableRooms: true,
+}).partial();
+
+export type UpdateHotelDTO = z.infer<typeof UpdateHotelDTO>;
+
+export const DeleteHotelDTO = z.object({
+  id: z.string().uuid(),
+});
+
+export type DeleteHotelDTO = z.infer<typeof DeleteHotelDTO>;
