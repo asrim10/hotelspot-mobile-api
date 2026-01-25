3c1bc94eeea1aadbf9f0c63cf904ac15ce180f5e
asrim10 <asrimmsuwall@gmail.com>
2026-01-25 09:08:30 +0545

    hotel_types

---
+++ src/types/hotel.types.ts
@@ -0,0 +1,14 @@
+import z from "zod";
+
+export const HotelSchema = z.object({
+  hotelname: z.string().min(2),
+  address: z.string().min(5),
+  city: z.string().min(2),
+  country: z.string().min(2),
+  rating: z.number().min(0).max(5).optional(),
+  description: z.string().optional(),
+  price: z.number().min(0),
+  availableRooms: z.number().min(0),
+});
+
+export type HotelType = z.infer<typeof HotelSchema>;
