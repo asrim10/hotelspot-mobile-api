3f5719920aa5c54acd410ab098307812bc7bd5a2
asrim10 <asrimmsuwall@gmail.com>
2026-01-25 09:17:17 +0545

    hotel_model

---
+++ src/models/hotel.model.ts
@@ -0,0 +1,26 @@
+import { HotelType } from "../types/hotel.types";
+
+import mongoose, { Document, Schema } from "mongoose";
+
+const HotelSchema: Schema = new Schema<HotelType>(
+  {
+    hotelname: { type: String, required: true },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+    rating: { type: Number, required: false },
+    description: { type: String, required: false },
+    price: { type: Number, required: true },
+    availableRooms: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
+export interface IHotel extends HotelType, Document {
+  _id: mongoose.Types.ObjectId;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export const HotelModel = mongoose.model<IHotel>("Hotel", HotelSchema);
